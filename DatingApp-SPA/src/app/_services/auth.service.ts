55782eaceada70ff71a02c46474fcc9dab54dfb6
brunocamps <bruno.mcamps@outlook.com>
2020-08-19 10:15:01 -0300

    Angular JWT to decode tokens

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,22 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({ // injectable decorator
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
 constructor(private http: HttpClient) {}
 
 login(model: any) {
-  return this.http.post(this.baseUrl + 'login', model)
+  return this.http
+  .post(this.baseUrl + 'login', model)
   .pipe(
     map((response: any) => {
       const user = response;
       if (user) {
         localStorage.setItem('token', user.token);
+        this.decodedToken = this.jwtHelper.decodeToken(user.token);
+        console.log(this.decodedToken); // this prints the token to the console
       }
     })
   );
@@ -27,7 +33,12 @@ login(model: any) {
 register(model: any) {
   // model is an object to store the username and pw
   return this.http.post(this.baseUrl + 'register', model);
+}
 
+loggedIn() {
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token); // need ! because it returns true
+  // if token is not expired, it returns true (!)
 }
 
 
